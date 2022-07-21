bc2fb11331b55712495af988ff59cc9cc51dfade
Youssef Achehboune <yachehbo@e3r8p3.1337.ma>
2022-07-21 21:30:09 +0100

    Structuring Code

---
+++ Experience/Camera.js
@@ -0,0 +1,55 @@
+import * as THREE from "three";
+import Experience from "./Experience.js";
+
+export default class Camera{
+    constructor(){
+        this.experience = new Experience();
+        this.sizes = this.experience.sizes;
+        this.scene = this.experience.scene;
+        this.canvas = this.experience.canvas;
+        
+        this.createPerspectiveCamera();
+        this.createOrthographicCamera();
+    }
+    createPerspectiveCamera(){
+        this.perspectiveCamera = new THREE.PerspectiveCamera(
+            35,
+            this.sizes.aspect,
+            0.1,
+            1000
+        );
+        this.perspectiveCamera.position.z = 5;
+        this.scene.add(this.perspectiveCamera);
+    }
+    createOrthographicCamera(){
+        this.frustrum
+        this.orthographicCamera = new THREE.OrthographicCamera(
+            (-this.sizes.aspect * this.sizes.frustrum)/2,
+            (this.sizes.aspect * this.sizes.frustrum)/2,
+            this.sizes.frustrum/2,
+            -this.sizes.frustrum/2,
+            -100,
+            100
+        );
+        this.scene.add(this.orthographicCamera)
+    }
+    resize(){
+        // Updating prespectiveCamera on Resize
+
+        this.perspectiveCamera.aspect = this.sizes.aspect;
+        this.perspectiveCamera.updateProjectionMatrix();
+        
+        // Updating orthgraphicCamera on Resize
+        
+        this.orthographicCamera.left = (-this.sizes.aspect * this.sizes.frustrum)/2;
+        this.orthographicCamera.right = (this.sizes.aspect * this.sizes.frustrum)/2;
+        this.orthographicCamera.top = this.sizes.frustrum/2;
+        this.orthographicCamera.bottom = -this.sizes.frustrum/2;
+        this.orthographicCamera.updateProjectionMatrix();
+
+    }
+
+    update(){
+
+    }
+}
--- Experience/Experience.js
+++ Experience/Experience.js
@@ -1,31 +1,44 @@
 import * as THREE from "three";
+import Sizes from "./Utils/Sizes.js";
+import Camera from "./Camera.js";
+import Renderer from "./Renderer.js";
+import Time from "./Utils/Time.js";
+import World from "./World/World.js";
 
 export default class Experience{
+    static instance
     constructor(canvas){
+        if(Experience.instance){
+            return Experience.instance
+        }
+        Experience.instance = this
         this.canvas = canvas;
-        const scene = new THREE.Scene();
-			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        this.scene = new THREE.Scene();
+        this.time = new Time();
+        this.sizes = new Sizes();
+        this.camera = new Camera();
+        this.renderer = new Renderer();
+        
+        this.world = new World();
 
-			const renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
-
-			const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			const cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
-
-			camera.position.z = 5;
-
-			function animate() {
-				requestAnimationFrame( animate );
-
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
-
-				renderer.render( scene, camera );
-			};
-
-			animate();
+        this.sizes.on("resize", ()=>{
+            this.resize();
+        })
+        
+        this.time.on("update", ()=>{
+            this.update();
+        })
+       
     }
+    
+    resize(){
+        this.camera.resize();
+        this.renderer.resize();
+    }
+
+    update(){
+        this.camera.update();
+        this.renderer.update();
+    }
+    
 }
