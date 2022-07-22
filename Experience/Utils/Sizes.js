b1654a982bfd2f1133659f0551d33e8cd5f7bbb8
Youssef Achehboune <yachehbo@e3r6p7.1337.ma>
2022-07-22 15:10:41 +0100

    fix pixelRatio in sizes class

--- Experience/Utils/Sizes.js
+++ Experience/Utils/Sizes.js
@@ -12,7 +12,7 @@ export default class Sizes extends EventEmitter{
             this.width = window.innerWidth;
             this.height = window.innerHeight;
             this.aspect = this.width/this.height;
-            this.pixelRatio = Math.min(window.devicepixelRatio, 2);
+            this.pixelRatio = Math.min(window.devicePixelRatio, 2);
             this.emit("resize")
         })
     
